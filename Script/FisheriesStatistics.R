c3deba82265227969d056fac2f9c5bda324e96e9
Marco Kienzle <marco.kienzle@gmail.com>
2019-10-19 11:13:22 +1300

    Added a plot of trends in CPUE

--- Script/FisheriesStatistics.R
+++ Script/FisheriesStatistics.R
@@ -1,5 +1,5 @@
 # CREATED    5 Oct. 2016
-# MODIFIED   5 Oct. 2016
+# MODIFIED  19 Oct. 2016
 
 # PURPOSE plot fisheries statistics
 
@@ -7,13 +7,16 @@ catch <- read.csv("../Data/GarfishCatchData.csv")
 effort <- read.csv("../Data/GarfishEffortData.csv")
 
 #postscript("Results/Graphics/CatchAndEffortVariations.ps")
-png("Results/Graphics/CatchAndEffortVariations.png")
+png("Results/Graphics/CatchAndEffortVariations.png", width = 800, height = 480)
 #par(fg = "black", col.main = "black", col.axis = "black", col.lab = "black", cex = 1.5, cex.lab = 2, cex.axis = 1.5, cex.main = 2.5, mai = c(1.2, 1.8, 1.02, 1.5), mgp = c(4, 1, 0))
 
-par(mai = c(1.2, 1.8, 1.02, 1.5), mgp = c(4, 1, 0))
+
+
+par(mfrow=c(1,2))
+par(mai = c(0.6, 1, 0.5, 1.5), mgp = c(4, 1, 0))
 
 mp <- with(catch, barplot(t(1e-3 * Catch.in.kg), las = 1, beside = T, border = "lightgrey", col = "lightgrey",
-                        ylab = "Catch (tonnes)", ylim = c(0, 100), axes = FALSE, xlab = ""))
+                        ylab = "Catch (tonnes)", ylim = c(0, 120), axes = FALSE, xlab = ""))
 
 axis(side = 1, at = mp, label = catch$Year)
 axis(side = 2, at = seq(0, 100, 20), label = seq(0,100,20), las = 1)
@@ -30,5 +33,19 @@ axis(side = 4, at = seq(0, 100, 20), label = seq(0, 1000, 200), las = 1)
 mtext(expression(paste("Effort (", boat.days^-1, ")", sep = "")), side=4,line=3)
 box()
 #abline( h = seq(0,3e4, 5e3), lty = 2, col = "grey")
-legend(15, 100, legend = c("Catch", "Effort"), col = c("lightgrey", "black"), pch = c(15,19), cex = 1, bg = "white")
+legend(21, 115, legend = c("Catch", "Effort"), col = c("lightgrey", "black"), pch = c(15,19), cex = 1, bg = "white")
+
+## Plot CPUE
+par(mai = c(0.6, 1, 0.5, 0.5), mgp = c(4, 1, 0))
+plot( 1:length(catch$Year), catch$Catch.in.kg/effort$Effort.in.days,
+      pch = 19,
+      xlab = "Fishing year", ylab = "Catch per unit effort (kg/boat-day)",
+      las = 1, axes = FALSE, ylim = c(0,350));
+
+axis(1, at = seq(1, length(catch$Year))[c(TRUE,FALSE,FALSE)], catch$Year[c(TRUE,FALSE,FALSE)]);
+axis(2, las = 1);
+box();
+
+abline(h = seq(0,300,50), col = "lightgrey")
+
 dev.off()
